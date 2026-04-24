f3fdec1dd1cf39185981517f67cdb312cedb882a
Bent Stange <bent.st@gmx.de>
2026-04-24 10:36:23 +0200

    .

---
+++ schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Fundsachen(
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    found_date INTEGER NOT NULL,
+    location TEXT NOT NULL,
+    status TEXT
+);
